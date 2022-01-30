4019e03310ec2eadbeea1c535695f9a487f3cd34
Daphne Adams <daphadams@gmail.com>
2022-01-29 18:34:38 -0800

    Week 3 Assignment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,16 +6,17 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { CAMPSITES } from '../shared/campsites';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { CAMPSITES } from '../shared/campsites'; //arrays of objects
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
 class Main extends Component {
-    constructor(props) {
+    constructor(props) { //sets up the default data available for the component
         super(props);
-        this.state = {
+        this.state = { //wtihin that component
             campsites: CAMPSITES,
             comments: COMMENTS,
             partners: PARTNERS,
@@ -26,7 +27,7 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return (
-                <Home 
+                <Home
                     campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
                     promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
                     partner={this.state.partners.filter(partner => partner.featured)[0]}
@@ -34,9 +35,9 @@ class Main extends Component {
             );
         };
 
-        const CampsiteWithId = ({match}) => {
+        const CampsiteWithId = ({ match }) => {
             return (
-                <CampsiteInfo 
+                <CampsiteInfo
                     campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
                     comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
                 />
@@ -49,10 +50,11 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
-                </Switch>
+                </Switch>  
                 <Footer />
             </div>
         );
